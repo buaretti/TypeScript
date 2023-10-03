8dff54f60b8f5705e0cae9b0d52339a70dd91149
Marcelo Buaretti <mbuaretti@gmail.com>
2023-10-03 10:08:52 -0300

    .

---
+++ modulo-01/typeAnnotation/typeAnnotation.js
@@ -0,0 +1,6 @@
+"use strict";
+// Variáveis [Type Annotations]
+let nome = "Marcelo Manoel";
+console.log(nome);
+let animais = ["cachorro", "panda", "verme", "girafa"];
+console.log(animais);
---
+++ modulo-01/typeAnnotation/typeAnnotation.ts
@@ -0,0 +1,6 @@
+// Variáveis [Type Annotations]
+let nome: string = "Marcelo Manoel"
+console.log(nome)
+
+let animais: string[] = ["cachorro", "panda", "verme", "girafa"]
+console.log(animais)
