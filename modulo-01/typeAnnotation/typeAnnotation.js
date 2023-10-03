3313d11c65ca6361b1bb5ede6c8da592c76ac74d
Marcelo Buaretti <mbuaretti@gmail.com>
2023-10-03 10:20:53 -0300

    .

--- modulo-01/typeAnnotation/typeAnnotation.js
+++ modulo-01/typeAnnotation/typeAnnotation.js
@@ -4,3 +4,6 @@ let nome = "Marcelo Manoel";
 console.log(nome);
 let animais = ["cachorro", "panda", "verme", "girafa"];
 console.log(animais);
+let carro;
+carro = { nome: "Porsche 718 Cayman", ano: 2020, preco: 500.000 };
+console.log(carro);
--- modulo-01/typeAnnotation/typeAnnotation.ts
+++ modulo-01/typeAnnotation/typeAnnotation.ts
@@ -4,3 +4,12 @@ console.log(nome)
 
 let animais: string[] = ["cachorro", "panda", "verme", "girafa"]
 console.log(animais)
+
+let carro: {
+    nome : string
+    ano : number
+    preco : number
+}
+
+carro = {nome: "Porsche 718 Cayman", ano: 2020, preco:500.000 }
+console.log(carro)
